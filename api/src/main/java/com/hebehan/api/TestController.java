498b7129252ee9866ec28b99308ac35e95a25851
HebeHan <doveskila@gmail.com>
2019-01-09 14:02:39 +0800

    init

---
+++ api/src/main/java/com/hebehan/api/TestController.java
@@ -0,0 +1,15 @@
+package com.hebehan.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class TestController {
+
+    @RequestMapping(value = "test",method = RequestMethod.GET)
+    public String test(){
+        return "hello world";
+    }
+}
